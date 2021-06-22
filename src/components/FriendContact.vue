0641c2352f900dd6ef863915aa566c9ca6b4acd5
Shynu Mary Varghese <Svarghese2938@conestogac.on.ca>
2021-06-22 18:10:49 +0530

    using components FriendContact

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,45 @@
+<template>
+    <li>
+        <h2> {{friend.name}} </h2>
+         <button @click="toggleDetails">{{ dataAreVisible ? 'Hide' : 'Show' }} Details</button>
+        <ul v-if="dataAreVisible">
+    <li >        
+<strong>
+    Phone:
+</strong>
+{{friend.phone}}
+    </li>
+<li>
+                <strong>
+                    Email:
+                </strong>
+{{friend.email}}
+            </li>
+        </ul>
+    </li>
+</template>
+<script>
+
+export default {
+   data(){
+       return{
+           dataAreVisible: false,
+           friend:{
+               id:'manuel',
+                  name:'Manuel Lorenz',
+                  phone: '0123 45678 90',
+                  email: 'manuel@localhost.com'
+           }
+       } ;  
+   },
+   methods: {
+          toggleDetails(){
+              this.dataAreVisible = !this.dataAreVisible;
+          } 
+       }
+  
+   
+};
+ 
+
+</script>
